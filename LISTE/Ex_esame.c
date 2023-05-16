8f0b41a6f9be8a4ab1ed7942f5da25e8be0881bc
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2023-05-16 22:43:10 +0200

    All commented to avoid error

--- LISTE/Ex_esame.c
+++ LISTE/Ex_esame.c
@@ -2,7 +2,7 @@
 lista. Si analizzi e motivi la complessità in spazio e in tempo.
 Es: 5->3->7->1->2->NULL   k = 3 Risultato 5*/
 
-int media_it(Nodo* head, int k){
+/*int media_it(Nodo* head, int k){
     int i=0, media=0;
     for(i=0; i<k; i++){
         media+=head->data;
@@ -10,13 +10,13 @@ int media_it(Nodo* head, int k){
     }
     media/=i;
     return media;
-}
+}*/
 
 /*Scrivere una funzione ricorsiva che prenda in input una lista e un intero k positivo e che cancelli dalla lista tutti
 i nodi a valore k, solo se sono in una posizione pari. Si analizzi e motivi la complessità in spazio e in tempo.
 Es: 5->3->7->1->3->NULL k = 3 Risultato 5->7->1->3->NULL*/
                                       //p=0;  
-void cancella_k_rc(Nodo* head, int k, int p){
+/*void cancella_k_rc(Nodo* head, int k, int p){
     if(head!=NULL){
         if((p%2==0)&&(head->data==k)){
             Nodo* new_head=head;
@@ -25,4 +25,4 @@ void cancella_k_rc(Nodo* head, int k, int p){
         }
         cancella_k_rc(head->next, k, p+1);
     }
-}
+}*/
