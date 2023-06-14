78ab2c93a62fbbaf1ee7d71222c9290d9d0e3ffc
Alessandro Zappatore <alessandrozappatore03@gmail.com>
2023-06-14 15:25:10 +0200

    Ex 18 modified

--- EX ESAME/22-esempi_ed_esercizi/Slide19.c
+++ EX ESAME/22-esempi_ed_esercizi/Slide19.c
@@ -8,17 +8,11 @@ struct nodo{
 
 struct nodo* build_list(int v[]);
 void print_list(struct nodo* head);
-int conta3(struct nodo* head);
-void duplicate(struct nodo* head, int x, int n);
+
 
 int main(){
-    int v[]={2,3,1,6,1,2,2,6,1,5,5,5,5,-1};
+    int v[]={2,2,5,8,1,2,2,4,1,5,-1};
     struct nodo* headlist=build_list(v);
-    print_list(headlist);
-
-    printf("I valori che si ripetono per tre volte sono: %d\n", conta3(headlist));
-
-    duplicate(headlist, 1, 3);
     print_list(headlist);
     return 0;
 }
@@ -51,28 +45,4 @@ void print_list(struct nodo* head){
 
     printf("%d -> ", head->data);
     print_list(head->next);
-}
-
-int conta3(struct nodo* head){
-    int c=0;
-
-    while(head->next->next!=NULL){
-        if(head->data==head->next->data&&head->data==head->next->next->data)
-            c++;
-        head=head->next;
-    }
-    return c;
-}
-
-void duplicate(struct nodo* head, int x, int n){
-    struct nodo* tmp=NULL;
-    for(int i=1; head!=NULL; i++){
-        if(head->data==x&&i%n==0){
-            tmp=(struct nodo*)malloc(sizeof(struct nodo));
-            tmp->data=head->data;
-            tmp->next=head->next;
-            head->next=tmp;
-        }
-        head=head->next;
-    }
 }
